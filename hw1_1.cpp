0e2ca4190382665b46d5c129f316f32bd4ebf77d
Mark Mariscal <markamariscal@gmail.com>
2020-01-07 12:48:00 -0800

    completed assignment

--- hw1_1.cpp
+++ hw1_1.cpp
@@ -24,6 +24,7 @@ int main()
   	int count; // gives us the count of the array length 
   
   	int temp, num1, num2; // temp number and are distance numbers  we store numbers in our array
+  	int num3,num4 = 0;
   	int min = 100; // min distance so a placeholder
     cout << "Enter input file name: "; //asks input
     cin >> fileName; // recieves input
@@ -55,17 +56,27 @@ int main()
       for (int j = i + 1; j < count; j++){ //is going from 1 to the end of the array.
         	temp = array[i] - array[j]; // this is making a temp int of the differences of the array
             temp = abs(temp);//makes abs value of temp value
+        	
         	if( temp < min && temp > 0){ // if the temp value is less then 100 and more than 0
             min = temp; // min becomes the temp value 
             num1 = array[i];//num1 becomes the value i in array
             num2 = array[j];//num2 becomes the value j in array
           }
+    		if( temp == min ){ 
+            min = temp; // min becomes the temp value 
+            num3 = array[i];//num3 becomes the value i in array
+            num4 = array[j];//num4 becomes the value j in array
+          }
+
       }
+    
     }
   
   	//print files to screen
   	cout << "Min distance: " << min << endl;
-  	cout << "Two numbers for min distance: " << num1 << " and " << num2;
+  	cout << "Two numbers for min distance: " << num1 << " and " << num2 << endl;
+
+  	cout << "Two numbers for min distance: " << num3 << " and " << num4;
   	delete[] array; //deletes arrray
     cout << "\n is closed";
 
