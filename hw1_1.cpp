820b36211522ecfc0214b4fb068fdbd839f94676
Mark Mariscal <markamariscal@gmail.com>
2020-01-08 20:17:11 -0800

    final

--- hw1_1.cpp
+++ hw1_1.cpp
@@ -16,16 +16,17 @@ using namespace std;
 
 int main()
 {
+
     ifstream myFile;
     string fileName;
   
-  	int *array; //pointer with array
+    int *array; //pointer with array
     int num;
-  	int count; // gives us the count of the array length 
+    int count; // gives us the count of the array length 
   
-  	int temp, num1, num2; // temp number and are distance numbers  we store numbers in our array
-  	int num3,num4 = 0;
-  	int min = 100; // min distance so a placeholder
+    int temp, num1, num2; // temp number and are distance numbers  we store numbers in our array
+    int num3,num4 = 0;
+    int min = 100; // min distance so a placeholder
     cout << "Enter input file name: "; //asks input
     cin >> fileName; // recieves input
 
@@ -33,51 +34,72 @@ int main()
     myFile.open(fileName.c_str()); //opens the file
 
     cout<< fileName + " is opened" << endl; //tells user file is open
-  	cout << "Reading count into array" <<endl; //reads count into array
+    cout << "Reading count into array" <<endl; //reads count into array
     if (myFile >> num) // if there is an input then count reads first line
     {
       count = num;
     }
   
-  	array = new int [count]; //array with count , create size of array
-  	count = 0;
+    array = new int [count]; //array with count , create size of array
+    count = 0;
     while(myFile >> num) //start after first line
     {
         cout << "Reading data from file. \n";
         array[count] = num;//adds number to array
-				cout<< num << endl; // Prints numbers.
-      	count++;
-	    
+        cout<< num << endl; // Prints numbers.
+        count++;
+      
     }
     myFile.close();//closes file 
-    cout << "logic in array" << endl;
+
+
    //This is how we are checking for the distance:
-  	for (int i = 0; i < count - 1; i++){ //is going from 0 to end - 1 in the array
+    for (int i = 0; i < count - 1; i++){ //is going from 0 to end - 1 in the array
       for (int j = i + 1; j < count; j++){ //is going from 1 to the end of the array.
-        	temp = array[i] - array[j]; // this is making a temp int of the differences of the array
+          temp = array[i] - array[j]; // this is making a temp int of the differences of the array so say this is 5
             temp = abs(temp);//makes abs value of temp value
-        	
-        	if( temp < min && temp > 0){ // if the temp value is less then 100 and more than 0
+
+            //temp is 5 <100 temp is > 0
+            //min becomes the temp number so min becomes now 5.
+            //so min is 5
+            //num1 is the distance numbers num1 is 50 and num2 is 55
+          
+          if( temp < min && temp > 0){ // if the temp value is less then 100 and more than 0
             min = temp; // min becomes the temp value 
             num1 = array[i];//num1 becomes the value i in array
             num2 = array[j];//num2 becomes the value j in array
           }
-    		if( temp == min ){ 
-            min = temp; // min becomes the temp value 
+          
+          //temp is 5 <100 temp is > 0
+          //min becomes the temp number so min becomes now 5.
+          //so min is 5
+          //num1 is the distance in the array at 50
+          //num2 is the distance in the array at 55
+
+          //if 5 = 5
+          //min becomes the temp so min is 5
+          //num3 is the distance in the array at 50
+          //num4 is the distance in the array at 55
+        if( temp == min && num1 != array[i]){ 
             num3 = array[i];//num3 becomes the value i in array
             num4 = array[j];//num4 becomes the value j in array
-          }
-
+           
+          }     
+    
       }
     
     }
   
-  	//print files to screen
-  	cout << "Min distance: " << min << endl;
-  	cout << "Two numbers for min distance: " << num1 << " and " << num2 << endl;
+    //print files to screen
+    cout << "Min distance: " << min << endl;
+    cout << "Two numbers for min distance: " << num1 << " and " << num2 << endl;
 
-  	cout << "Two numbers for min distance: " << num3 << " and " << num4;
-  	delete[] array; //deletes arrray
+ if( num3 != num4){
+      cout << "Two numbers for min distance: " << num3 << " and " << num4 << endl;
+   }
+
+
+    delete[] array; //deletes arrray
     cout << "\n is closed";
 
     return 0;
